065ce5b2a2a29ff86852b84ccc9984b0da3f6f83
Napat <ton_lakla2548@hotmail.com>
2016-10-15 10:58:11 +0700

    week3

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const title = 'My React App'
+
+const App = () => {
+
+    return <div> My React App </div>
+
+}
+
+const element = document.getElementById('app')
+ReactDOM.render(<App />, element)
