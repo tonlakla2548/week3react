92b278f3602a13cdbe2d9ba7a3e4fdd3d05224b8
Napat <ton_lakla2548@hotmail.com>
2016-10-21 19:06:03 +0700

    react pokedex

--- src/app.js
+++ src/app.js
@@ -1,59 +1,75 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-const SearchForm = () => {
-    return (
-        <form>
-            <input type="text" />
-            <button type="submit">search</button>
-        </form>
-    )
-}
+import ReactTable from 'react-table'
 
-const Header = (props) => (
+
+
+const Header =(props) => (
     <header>
-        <h1>{props.title}</h1>
-        <SearchForm />
+        <p>{props.title}</p>
+
     </header>
 )
+
 const Items = (props) => {
     console.log(props.items)
     return (
-        <ul>
-        {props.items.map(item => (
-            <li>{item}</li>
-        ))}
-        </ul>
+      <table>
+      <tr>
+      <th>No</th>
+          <th>Pic</th>
+          <th>Name</th>
+          <th>Element</th>
+      <th>Skill</th>
+       </tr>
+
+                   {props.items.map(item => (
+                 <tr>
+                         <td>{item[0]} </td>
+
+                         <td><img src={item[1]} alt="HTML5 Icon" width="120" height="120"/></td>
+                         <td>{item[2]} </td>
+                         <td>{item[3]} </td>
+                         <td>{item[4]} </td>
+                 </tr>
+                 ))}
+
+
+        </table>
     )
 }
 
 const Content = (props) => (
     <section>
-        <p>{props.description}</p>
+
         <Items items={props.items}/>
     </section>
 )
 
-const AppWithoutDescription = () => (
-    <Header title="No description here" />
-
-)
 
 const App = () => {
-    const appTitle = 'Fronttechs: React'
-    const description = 'A simple react application'
-  const items =[
-      "Olives" ,
-      "Tobey",
-      "Charlie",
-      "Lucky",
-      "Poyo"
+    const appTitle = 'Mypokedex'
+
+    const items =[
+
+    [ "01","001.png","Bulbasaur", "GRASS & POISON" ,"Tackle,Vine Whip"],
+    [ "02","002.png","Ivysaur", "GRASS & POISON" ,"Razor Leaf,Vine Whip"],
+    [ "03","003.png","Venusaur", "GRASS & POISON" ,"Razor Leaf,Vine Whip"],
+    [ "04","004.png","Charmander",  "FIRE " ,"Scratch,Ember"],
+    [ "05","005.png","Charmeleon",  "FIRE " ,"Scratch,Ember"],
+    [ "06","006.png","Charizard", "FIRE & FLYING" ,"Wing Attack,Ember"],
+    [ "07","007.png","Squirtle", "WATER" ,"Bite,Water Gun"],
+    [ "08","008.png","Wartortle","WATER" ,"Bite,Water Gun"],
+    [ "09","009.png","Blastoise", "WATER" ,"Bite,Water Gun"],
+    [ "10","010.png","Caterpie", "BUG" ,"Tackle,Bug Bite"]
     ]
     return (
         <section>
             <Header title={appTitle} />
             <Content
-              description ={description}
-              items={items}
+
+            items={items}
+
             />
         </section>
     )
